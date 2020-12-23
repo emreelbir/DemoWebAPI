1bfe3cd967f265a1b9cd2086fec53c9af83256fd
Emre Elbir <59312457+emreelbir@users.noreply.github.com>
2020-12-23 15:53:19 +0100

    Add project files.

---
+++ WebApi/Controllers/PeopleController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+
+    /// <summary>
+    /// This is where I give you all the information about my people.
+    /// </summary>
+
+
+    public class PeopleController : ApiController
+    {
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Emre", LastName = "Elbir", Id = 1 });
+            people.Add(new Person { FirstName = "Salih", LastName = "Yilmaz", Id = 2 });
+            people.Add(new Person { FirstName = "Sude", LastName = "Eren", Id =  3});
+        }
+        /// <summary>
+        /// Gets a list of the first names of all users.
+        /// </summary>
+        /// <param name="userId">The unique identier for this person</param>
+        /// <param name="age">We want to know how old they are.</param>
+        /// <returns>list of first names</returns>
+        [Route("api/People/GetFirstNames/{userId:int}/{age:int}")]
+        [HttpGet]
+        public List<string> GetFirstNames(int userId, int age)
+        {
+            List<string> output = new List<string>();
+
+            foreach(var p in people)
+            {
+                output.Add(p.FirstName);
+            }
+             
+            return output;
+        }
+
+        // GET: api/People
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            return people.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/People
+        public void Post(Person val)
+        {
+            people.Add(val);
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+            
+        }
+    }
+}
